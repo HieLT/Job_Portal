658e6a225aa09f95b965c44f1bfc4fc41eece13f
badliar0812 <buiquangtuan0812@gmail.com>
2024-07-18 15:55:23 +0700

    Update feature: Candidate upload resume, avatar

--- server/src/config/firsebase.ts
+++ server/src/config/firsebase.ts
@@ -3,12 +3,10 @@ import { config } from "dotenv";
 config();
 
 const serviceAccount = require('../../job-portal-429605-firebase-adminsdk-gelew-e0479275ec.json');
-
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     storageBucket: process.env.STORAGE_BUCKET,
 });
 
 const bucket = admin.storage().bucket();
-
 export default bucket;
