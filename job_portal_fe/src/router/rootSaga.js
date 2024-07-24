4064a071b8b921afc512f430ccbcf501c688fcd5
Hiep <lethanhhiep430@gmail.com>
2024-07-24 23:45:40 +0700

    fix login 2 times

--- job_portal_fe/src/router/rootSaga.js
+++ job_portal_fe/src/router/rootSaga.js
@@ -1,8 +1,10 @@
 import loadAuthSaga from "../states/modules/auth/saga.js";
 import loadHomeSaga from "../states/modules/home/saga.js";
 import loadProfileSaga from "../states/modules/profile/saga.js";
+import loadAdminSaga from "../states/modules/admin/saga.js";
 
 export const ROUTE_SAGAS = [];
 ROUTE_SAGAS['LOAD_HOME_PAGE'] = loadHomeSaga
 ROUTE_SAGAS['LOAD_AUTH_PAGE'] = loadAuthSaga
 ROUTE_SAGAS['LOAD_PROFILE_PAGE'] = loadProfileSaga
+ROUTE_SAGAS['LOAD_ADMIN_PAGE'] = loadAdminSaga
