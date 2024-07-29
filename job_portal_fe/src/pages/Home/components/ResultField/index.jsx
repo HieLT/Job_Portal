a264187174e0cc133f13106d49a9b3ccd2ea8e99
Hiep <lethanhhiep430@gmail.com>
2024-07-30 01:13:36 +0700

    Squash commit

--- job_portal_fe/src/pages/Home/components/ResultField/index.jsx
+++ job_portal_fe/src/pages/Home/components/ResultField/index.jsx
@@ -2,31 +2,42 @@ import React from 'react';
 import styles from './styles.module.scss';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+
+
 const formatDate = (dateString) => {
   const date = new Date(dateString);
   return date.toISOString().split('T')[0];
 };
+
+
+
+
 const ResultField = () => {
-
   const jobs = useSelector((state) => state.home.jobs);
+  const isJobExpired = (expiryDateString) => {
+    const expiryDate = new Date(expiryDateString);
+    const today = new Date();
+    return expiryDate < today;
+  };
 
-
-
+  console.log(jobs);
   return (
     <div className={styles.resultField}>
-      {jobs.map((job) => (
-        <Link key={job._id} to={`/job/${job._id}`} className={styles.jobCard}>
-          <div className={styles.jobHeader}>
-            <h3 className={styles.jobTitle}>{job.title}</h3>
-            <p className={styles.jobCompany}>{job.company}</p>
-          </div>
-          <div className={styles.jobDetails}>
-            <p className={styles.jobType}>{job.type}</p>
-            <p className={styles.jobDate}>Ngày đăng: {formatDate(job.createdAt)}</p>
-            <p className={styles.jobSalary}>Lương: {job.salary}</p>
-          </div>
-        </Link>
-      ))}
+      {jobs
+        .filter((job) => !isJobExpired(job.expired_at) && job.status === 'Open')  
+        .map((job) => (
+          <Link key={job._id} to={`/job/${job._id}`} className={styles.jobCard}>
+            <div className={styles.jobHeader}>
+              <h3 className={styles.jobTitle}>{job.title}</h3>
+              <p className={styles.jobCompany}>{job.company}</p>
+            </div>
+            <div className={styles.jobDetails}>
+              <p className={styles.jobType}>{job.type}</p>
+              <p className={styles.jobDate}>Ngày đăng: {formatDate(job.createdAt)}</p>
+              <p className={styles.jobSalary}>Lương: {job.salary}</p>
+            </div>
+          </Link>
+        ))}
     </div>
   );
 };
